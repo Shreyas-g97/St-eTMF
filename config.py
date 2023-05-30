7796edceb6152c07eca548751b3bb902ddb7313b
Shreyas Garimella <shreyas@adaptichealth.io>
2023-05-30 01:40:02 -0400

    changed config

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 OPENAI_API_KEY = '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'
-DATABASE_PASS = #insert your database password here as a string (e.g. 'password'). This is the password you created when you installed MySQL
+DATABASE_PASS = 'Godzilla197'
